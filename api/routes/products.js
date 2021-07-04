2dff5f2201cbddb1530bdc13108f8f09473c0001
Ashwin Singh <ashwins9001@gmail.com>
2021-07-04 16:30:57 -0500

    error handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -36,7 +36,7 @@ router.patch('/:productId', (req, res, next) => {
 router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'deleted product!'
-    })
+    }) 
 });
 
 module.exports = router;
